e04bc748ff8bf73b63dadc7220537aa3b646abd2
stouma915 <sasakitouma@yahoo.ne.jp>
2022-08-27 15:53:31 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,6 +33,13 @@ function Main() {
                     )
                 }
             </div>
+            <div>
+                <p>
+                    This site is licensed under the <a href="https://github.com/stouma915/minesweeper/blob/main/LICENSE">MIT License</a>.<br />
+                    This site is open source. <a href="https://github.com/stouma915/minesweeper/">Improve this site</a>.<br />
+                    Powered by <a href="https://pages.github.com">GitHub Pages</a>.
+                </p>
+            </div>
         </>
     );
 }
