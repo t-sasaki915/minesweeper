c227be16c7e15867f6412ccb3bf4c1cc174b537f
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-01 12:15:36 +0900

    Create AboutPage.tsx

---
+++ components/AboutPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <p>
+                This site is licensed under the <a href="https://github.com/stouma915/minesweeper/blob/main/LICENSE">MIT License</a>.<br />
+                This site is open source. <a href="https://github.com/stouma915/minesweeper/">Improve this site</a>.<br />
+                Powered by <a href="https://pages.github.com">GitHub Pages</a>.
+            </p>
+        </div>
+    );
+};
+
+export default AboutPage;
