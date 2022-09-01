ad081926584156bbabfad554ad97e485920cf3cf
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-01 10:47:19 +0900

    Update DifficultySelect.tsx

--- components/DifficultySelect.tsx
+++ components/DifficultySelect.tsx
@@ -12,8 +12,12 @@ function setDifficulty(w: Window, difficulty: string): void {
 
 const DifficultySelect = (w: Window) => {
     return (
-        <a href="#" onClick={() => setDifficulty(w, "easy")}>easy</a><br />
-        <a href="#" onClick={() => setDifficulty(w, "normal")}>normal</a><br />
-        <a href="#" onClick={() => setDifficulty(w, "hard")}>hard</a>
+        <div>
+            <a href="#" onClick={() => setDifficulty(w, "easy")}>easy</a><br />
+            <a href="#" onClick={() => setDifficulty(w, "normal")}>normal</a><br />
+            <a href="#" onClick={() => setDifficulty(w, "hard")}>hard</a>
+        </div>
     );
 };
+
+export default DifficultySelect;
