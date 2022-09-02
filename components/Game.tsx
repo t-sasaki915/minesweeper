3756a633eddd7b23319996ee6aa5cc9aea913449
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-02 09:11:20 +0900

    Update Game.tsx

--- components/Game.tsx
+++ components/Game.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
 
-const Game = () => {
+import { Util } from "../src/ts/util";
+
+type Props = {
+    width: number;
+    height: number;
+    cellClicked: (x: number, y: number) => void;
+    cellRightClicked: (x: number, y: number) => void;
+}
+
+const Game = (props: Props) => {
     return (
-        <div>
+        <div className="game">
+            {
+                Util.range(0, props.height).map(y =>
+                    <div className="line">
+                        { 
+                            Util.range(0, props.width).map(x =>
+                                <div className="cell cell-not-opened"
+                                     id={x.toString() + "-" + y.toString()}
+                                     draggable="false"
+                                     onClick={() => props.cellClicked(x, y)}
+                                     onContextMenu={(e) => {
+                                         e.preventDefault();
+                                         props.cellRightClicked(x, y);
+                                     }}
+                                >0</div>
+                            )
+                        }
+                    </div>
+                )
+            }
         </div>
     );
 };
