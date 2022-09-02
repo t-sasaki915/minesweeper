a96c6ca802c9b7ba0e49b74b460a88242e9859ea
stouma915 <sasakitouma@yahoo.ne.jp>
2022-09-02 16:33:17 +0900

    Update DifficultySelect .tsx

--- components/DifficultySelect.tsx
+++ components/DifficultySelect.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { DIFFICULTIES } from "../src/ts/difficulty";
+
 function setDifficulty(difficulty: string): void {
     const url = new URL(window.location.href);
 
@@ -13,9 +15,11 @@ function setDifficulty(difficulty: string): void {
 const DifficultySelect = () => {
     return (
         <div>
-            <a href="#" onClick={() => setDifficulty("easy")}>easy</a><br />
-            <a href="#" onClick={() => setDifficulty("normal")}>normal</a><br />
-            <a href="#" onClick={() => setDifficulty("hard")}>hard</a>
+            {
+                DIFFICULTIES.map(d =>
+                    <a href="#" onClick={() => setDifficulty(d.id())}>{d.id()}<br /></a>
+                )
+            }
         </div>
     );
 };
