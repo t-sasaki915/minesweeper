e0ae6be1086cfbc5823d979add69ef62464f28a5
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-02 09:25:11 +0900

    Update util.ts

--- src/ts/util.ts
+++ src/ts/util.ts
@@ -13,31 +13,3 @@ export function cellElemAt(x: number, y: number): HTMLElement {
 export function cellElemAt_(coord: Coordinate): HTMLElement {
     return cellElemAt(coord.x(), coord.y());
 }
-
-export class Coordinate {
-
-    private _x: number;
-    private _y: number;
-
-    constructor(x: number, y: number) {
-        this._x = x;
-        this._y = y;
-    }
-
-    public x(): number {
-        return this._x;
-    }
-
-    public y(): number {
-        return this._y;
-    }
-
-    public equals(x: number, y: number): boolean {
-        return this.x() == x && this.y() == y;
-    }
-
-    public equals_(coord: Coordinate): boolean {
-        return this.equals(coord.x(), coord.y());
-    }
-
-}
