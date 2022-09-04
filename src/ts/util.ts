f1a1c7841337fdcb2e789c82c089b581b7ca03bc
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-05 08:48:43 +0900

    Update util.ts

--- src/ts/util.ts
+++ src/ts/util.ts
@@ -1,4 +1,5 @@
 import Coordinate from "./coordinate";
+import { Cell } from "./minesweeper";
 
 export function range(min: number, max: number): Array<number> {
     return Array.from(Array(max - min).keys()).map(x => x + min);
@@ -14,4 +15,33 @@ export function cellElemAt(x: number, y: number): HTMLElement {
 
 export function cellElemAt_(coord: Coordinate): HTMLElement {
     return cellElemAt(coord.x(), coord.y());
+}
+
+export function nearCells(
+    center: Coordinate,
+    width: number,
+    height: number
+): Array<Cell> {
+    const centerX = center.x();
+    const centerY = center.y();
+
+    const nearCells = [];
+
+    for (let i = -1; i < 2; i ++) {
+        for (let j = -1; j < 2; j ++) {
+            const nx = centerX + i;
+            const ny = centerY + j;
+
+            if (nx == centerX && ny == centerY) {
+                continue;
+            }
+            if (nx < 0 || nx >= width || ny < 0 || ny >= height) {
+                continue;
+            }
+
+            nearCells.push(new Coordinate(nx, ny));
+        }
+    }
+
+    return nearCells;
 }
