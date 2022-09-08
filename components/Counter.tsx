55aaef44015eea4187dee3bef28f9edb7cc6d78d
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-09 08:00:26 +0900

    Update Counter.tsx

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -1,35 +1,39 @@
 import React from 'react';
 
+import { IllegalStateError } from '../src/ts/errors';
+
 const COUNTS = new Map<string, number>();
 
 function getElement(id: string): HTMLElement {
     return document.getElementById(`counter-${id}`)!;
 }
 
-export function addCount(id: string, n: number = 1): void {
-    if (COUNTS.has(id)) {
-        const count = COUNTS.get(id)!;
-
-        getElement(id).innerHTML = `${count + n}`;
-
-        COUNTS.set(id, count + n);
+export function addCount(id: string, n: number): void {
+    if (!COUNTS.has(id)) {
+        throw new IllegalStateError(`unknown counter: ${id}`);
     }
+
+    const count = COUNTS.get(id)!;
+    getElement(id).innerHTML = `${count + n}`;
+    COUNTS.set(id, count + n);
 }
 
 export function setCount(id: string, n: number): void {
-    if (COUNTS.has(id)) {
-        getElement(id).innerHTML = `${n}`;
-
-        COUNTS.set(id, n);
+    if (!COUNTS.has(id)) {
+        throw new IllegalStateError(`unknown counter: ${id}`);
     }
+
+    getElement(id).innerHTML = `${n}`;
+    COUNTS.set(id, n);
 }
 
 export function resetCount(id: string): void {
-    if (COUNTS.has(id)) {
-        getElement(id).innerHTML = "0";
-
-        COUNTS.set(id, 0);
+    if (!COUNTS.has(id)) {
+        throw new IllegalStateError(`unknown counter: ${id}`);
     }
+
+    getElement(id).innerHTML = "0";
+    COUNTS.set(id, 0);
 }
 
 type Props = {
@@ -40,7 +44,7 @@ const Counter = (props: Props) => {
     COUNTS.set(props.id, 0);
 
     return (
-        <span id={`counter-${props.id}`}>0</span>
+        <span className="counter" id={`counter-${props.id}`}>0</span>
     );
 };
 
