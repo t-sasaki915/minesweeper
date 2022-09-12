1286cbedfe147807da64938c211b971b3137149a
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-12 12:58:13 +0900

    Create cell.ts

---
+++ src/ts/cell.ts
@@ -0,0 +1,37 @@
+abstract class Cell {
+
+    private _coord: Coordinate;
+
+    constructor(coord: Coordinate) {
+        this._coord = coord;
+    }
+
+    public coord(): Coordinate {
+        return this._coord;
+    }
+
+    public notMine(): boolean {
+        return !this.isMine();
+    }
+
+    public abstract isMine(): boolean;
+
+}
+
+export class NeutralCell extends Cell {
+
+    public isMine(): boolean {
+        return false;
+    }
+
+}
+
+export class MineCell extends Cell {
+
+    public isMine(): boolean {
+        return true;
+    }
+
+}
+
+export default Cell;
