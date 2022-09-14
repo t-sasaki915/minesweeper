156402c93392e611760f90c191f9576e840fd25d
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-14 10:18:18 +0900

    Update constants.ts

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -1,4 +1,4 @@
 import Coordinate from "./coordinate";
 
-export const IS_MINE: (coord: Coordinate, cells: Array<Coordinate>) =>
+export const IS_MINE: (coord: Coordinate, cells: Array<Coordinate>) => boolean =
   (coord, cells) => cells.some(c => c.equals_(coord))
