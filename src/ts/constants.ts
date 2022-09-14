c3a86261e0b796889b8d9e96e0fad65f48e3321e
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-14 10:21:52 +0900

    Update constants.ts

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -1,4 +1,8 @@
+import Cell from "./cell";
 import Coordinate from "./coordinate";
 
+export const TO_COORD_ARRAY: (arr: Array<Cell>) => Array<Coordinate> =
+  (arr) => arr.map(c => c.coord());
+
 export const IS_MINE: (coord: Coordinate, cells: Array<Coordinate>) => boolean =
-  (coord, cells) => cells.some(c => c.equals_(coord))
+  (coord, cells) => cells.some(c => c.equals_(coord));
