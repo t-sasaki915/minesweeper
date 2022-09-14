7b9588726020949c5b02ce9fa9d3590032b7f082
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-14 10:33:43 +0900

    Update constants.ts

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -1,8 +1,12 @@
 import Cell from "./cell";
 import Coordinate from "./coordinate";
+import Minesweeper from "./minesweeper";
 
 export const TO_COORD_ARRAY: (arr: Array<Cell>) => Array<Coordinate> =
   (arr) => arr.map(c => c.coord());
 
-export const IS_MINE: (coord: Coordinate, cells: Array<Coordinate>) => boolean =
-  (coord, cells) => cells.some(c => c.equals_(coord));
+export const IS_MINE: (x: number, y: number, instance: Minesweeper) => boolean =
+  (x, y, instance) => instance.cellAt(x, y).isMine()
+
+export const IS_MINE_: (coord: Coordinate, instance: Minesweeper) => boolean =
+  (coord, instance) => IS_MINE(coord.x(), coord.y(), instance);
