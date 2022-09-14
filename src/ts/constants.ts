5c95f3233488e279e8fa890bad8ac29d8c6aedb4
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-15 07:53:02 +0900

    Update constants.ts

--- src/ts/constants.ts
+++ src/ts/constants.ts
@@ -1,1 +1,11 @@
-export const ABOUT_PAGE_CLASS_NAME = "aboutPage";
+export const TOGGLE_FLAG_BUTTON_ID = "toggleFlag";
+export const TOGGLE_CHORD_BUTTON_ID = "toggleChord";
+export const RESTART_BUTTON_ID = "restart";
+
+export const CELL_NOT_OPENED_CLASS = "cell cell-not-opened";
+export const CELL_NUM_CLASS = "cell cell-num-";
+export const CELL_MINE_CLASS = "cell cell-mine";
+export const CELL_MINE_CAUSE_CLASS = "cell cell-mine-cause";
+export const CELL_FLAG_CLASS = "cell cell-flag";
+export const CELL_FLAG_PLACEHOLDER_CLASS = "cell cell-flag-placeholder";
+export const CELL_FLAG_MISS_CLASS = "cell cell-flag-miss";
