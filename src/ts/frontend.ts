a4a4a83d4eab06f083444d63beb855acf1baedbc
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-15 11:01:46 +0900

    Update frontend.ts

--- src/ts/frontend.ts
+++ src/ts/frontend.ts
@@ -1,1 +1,18 @@
+import Coordinate from "./coordinate";
+import GameContext from "./context";
 
+export function getCellElem(
+    coord: Coordinate,
+    context: GameContext
+): HTMLElement {
+    const id = `${context.name()}-${coord.x()}-${coord.y()}`;
+    return document.getElementById(id)!;
+}
+
+export function setCellClass(
+    newClass: string,
+    coord: Coordinate,
+    context: GameContext
+): void {
+    getCellElem(coord, context).className = newClass;
+}
