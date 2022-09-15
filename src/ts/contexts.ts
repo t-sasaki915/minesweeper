e5ac413f57e726251d0bf2f634110f334d635b61
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-15 10:54:50 +0900

    Update contexts.ts

--- src/ts/contexts.ts
+++ src/ts/contexts.ts
@@ -1,4 +1,22 @@
+import GameContext from "./context";
+
+const CONTEXTS: Array<GameContext> = [];
+
 class GameContexts {
+
+    constructor () { }
+
+    public static getContext(name: string): GameContext {
+        return CONTEXTS.filter(c => c.name() == name)[0];
+    }
+
+    public static createGame(name: string): GameContext {
+        const context = GameContext.inactiveContext(name);
+        CONTEXTS.push(context);
+
+        return this.getContext(name);
+    }
+
 }
 
 export default GameContexts;
