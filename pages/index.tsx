b31867fc841cb8eeeeb1f903204c82464c2f4f09
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-16 11:42:25 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,38 @@ import Difficulties, { Difficulty, EASY } from "../src/ts/difficulty";
 import { isOnBrowser } from "../src/ts/util";
 
 const GAME_ID = "main";
+const IMAGES_TO_LOAD = [
+    "cellNotOpened.png",
+    "flag.png",
+    "flagMiss.png",
+    "flagPlaceholder.png",
+    "mine.png"
+];
+
+let doneLoading = false;
+let loadingErr = false;
+
+function loadImages(): void {
+    const promises = IMAGES_TO_LOAD.map(url => {
+        return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.onload = () => resolve();
+            img.onerror = () => reject();
+            img.src = url;
+        });
+    });
+    
+    Promise.all(promises)
+        .then(() => {
+            loadingErr = false;
+        })
+        .catch(() => {
+            loadingErr = true;
+        })
+        .finally(() => {
+            doneLoading = true;
+        });
+}
 
 function Main() {
     if (isOnBrowser()) {
@@ -28,6 +60,23 @@ function Main() {
             }
 
             difficulty = Difficulties.get(diffParam)!;
+        }
+
+        loadImages();
+        
+        if (!doneLoading) {
+            return (
+                <>
+                    <p>loading images...</p>
+                </>
+            );
+        }
+        if (loadingErr) {
+            return (
+                <>
+                    <p>loading failed.</p>
+                </>
+            );
         }
 
         const context = GameContext.inactiveContext(GAME_ID, difficulty);
