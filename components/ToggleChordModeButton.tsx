480b12ad933fa436c8b048c443a274284b21af5c
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-09-20 10:56:12 +0900

    Update ToggleChordModeButton.tsx

--- components/ToggleChordModeButton.tsx
+++ components/ToggleChordModeButton.tsx
@@ -4,13 +4,13 @@ import GameContext from "../src/ts/context";
 
 import * as Logic from "../src/ts/game-logic";
 
-type Props = {
-    context: GameContext;
-}
-
 export function updateInnerHTML(context: GameContext): void {
     const elem = document.getElementById(`${context.name()}-toggleChordMode`)!;
     elem.innerHTML = context.chordMode() ? "exit chord mode" : "switch to chord mode";
+}
+
+type Props = {
+    context: GameContext;
 }
 
 const ToggleChordModeButton = (props: Props) => {
