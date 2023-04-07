81796f7a4951adee0ccb1bb3fdc2b3475372b590
Sasaki Touma <62248740+t-sasaki915@users.noreply.github.com>
2023-04-08 02:09:20 +0900

    Update AboutPage.tsx

--- components/AboutPage.tsx
+++ components/AboutPage.tsx
@@ -4,8 +4,8 @@ const AboutPage = () => {
     return (
         <div className="aboutPage">
             <p>
-                This site is licensed under the <a href="https://github.com/stouma915/minesweeper/blob/main/LICENSE">MIT License</a>.<br />
-                This site is open source. <a href="https://github.com/stouma915/minesweeper/">Improve this site</a>.<br />
+                This site is licensed under the <a href="https://github.com/t-sasaki915/minesweeper/blob/main/LICENSE">MIT License</a>.<br />
+                This site is open source. <a href="https://github.com/t-sasaki915/minesweeper/">Improve this site</a>.<br />
                 Powered by <a href="https://pages.github.com">GitHub Pages</a>.
             </p>
         </div>
